755cf3f0d10c6e177b69ec9906f13fe6d4b7e53e
Omar Garza <omaralejandrogarzav@gmail.com>
2022-07-29 20:31:03 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import FoodDropList from "./components/FoodDropList"
+import RecipiesTable from "./components/RecipiesTable"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section>
+          <FoodDropList />
+        </section>
+    
+        <section>
+        <RecipiesTable />
+        </section>
+      
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FoodDropList.js
@@ -0,0 +1,24 @@
+import React,{Component} from "react";
+//import './FoodDropList.css';
+
+class  FoodDropList extends Component {
+
+  render (){
+   return(
+      <div className="container" id="FoodDropList">
+        
+        <select id = "myList" onchange = "addIngredient()" >  
+          <option> ---Choose tutorial--- </option>  
+          <option> Cebolla </option>  
+          <option> Pan </option>  
+          <option> Tortillas </option>  
+          <option> Queso </option>  
+        </select>  
+
+      </div>
+   );
+  }
+
+}
+
+export default FoodDropList;
